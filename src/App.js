1814014087bf20f9525d1bbc34295603c198e6b8
vungoc.van.574 <vungoc.van.574@gmail.com>
2021-02-03 01:05:03 +0700

    10.02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
+import { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CellItem from './components/CellItem-10-02'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloading..........
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  
+  render() {
+    return (
+      <div className="App">
+        <CellItem />
+      </div>
+    );
+  }
 }
+
 
 export default App;
